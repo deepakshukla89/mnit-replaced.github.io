1d43ba9ebf4da414d48d90d6858ea53683cd3c09
Deepak Shukla <75180710+deepakshukla89@users.noreply.github.com>
2021-02-05 20:59:06 +0530

    Add files via upload

---
+++ js/acad.js
@@ -0,0 +1,35 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
+
+// people section
+
+const peoples = document.querySelectorAll('[data-people-target]')
+const peopleContents = document.querySelectorAll('[data-people-content]')
+
+peoples.forEach(people => {
+  people.addEventListener('click', () => {
+    const target = document.querySelector(people.dataset.peopleTarget)
+    peopleContents.forEach(peopleContent => {
+      peopleContent.classList.remove('active')
+    })
+    peoples.forEach(people => {
+      people.classList.remove('active')
+    })
+    people.classList.add('active')
+    target.classList.add('active')
+  })
+})
