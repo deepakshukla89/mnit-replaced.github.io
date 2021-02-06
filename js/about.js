5325ddef0bfebcb0863fcd662310ec47d730c2e8
Deepak Shukla <75180710+deepakshukla89@users.noreply.github.com>
2021-02-06 19:05:28 +0530

    Update about.js

--- js/about.js
+++ js/about.js
@@ -20,26 +20,26 @@ tabs.forEach(tab => {
 const peoples = document.querySelectorAll('[data-people-target]')
 const peopleContents = document.querySelectorAll('[data-people-content]')
 
-// peoples.forEach(people => {
-//   people.addEventListener('click', () => {
-//     const target = document.querySelector(people.dataset.peopleTarget)
-//     peopleContents.forEach(peopleContent => {
-//       peopleContent.classList.remove('active')
-//     })
-//     peoples.forEach(people => {
-//       people.classList.remove('active')
-//     })
-//     people.classList.add('active')
-//     target.classList.add('active')
-//   })
-// })
+peoples.forEach(people => {
+  people.addEventListener('click', () => {
+    const target = document.querySelector(people.dataset.peopleTarget)
+    peopleContents.forEach(peopleContent => {
+      peopleContent.classList.remove('active')
+    })
+    peoples.forEach(people => {
+      people.classList.remove('active')
+    })
+    people.classList.add('active')
+    target.classList.add('active')
+  })
+})
 
-(function($){
-  'use strict';
-    $(window).on('load', function () {
-        if ($(".pre-loader").length > 0)
-        {
-            $(".pre-loader").fadeOut("slow");
-        }
-    });
-})(jQuery)
+// (function($){
+//   'use strict';
+//     $(window).on('load', function () {
+//         if ($(".pre-loader").length > 0)
+//         {
+//             $(".pre-loader").fadeOut("slow");
+//         }
+//     });
+// })(jQuery)
